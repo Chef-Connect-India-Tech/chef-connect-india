c5047f1ff6702b86bb7c20ac84238682d1ae12da
Aravindreddy Mokireddy <aravindreddy.mokireddy@gmail.com>
2022-03-15 16:55:26 +0530

    packages removed

--- lib/Registration_user.dart
+++ lib/Registration_user.dart
@@ -5,7 +5,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:glass_morphism/Helper/model.dart';
-import 'package:glass_morphism/User_login.dart';
 import 'package:glass_morphism/home.dart';
 import 'package:glassmorphism/glassmorphism.dart';
 
