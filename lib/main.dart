4efa8aed06d200895058b938509ed54f3b32a38e
Aravindreddy Mokireddy <aravindreddy.mokireddy@gmail.com>
2022-03-23 01:20:13 +0530

    minor

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:glass_morphism/Main%20Screen/User_login.dart';
 import 'package:glass_morphism/roles/chef/chef_login.dart';
 // import 'package:glass_morphism/roles/user/User_d.dart';
-import 'package:glass_morphism/roles/user/user_dash.dart';
-import 'package:glass_morphism/roles/user/user_home.dart';
+// import 'package:glass_morphism/roles/user/user_home.dart';
 import 'package:glassmorphism/glassmorphism.dart';
 import 'package:firebase_core/firebase_core.dart';
 
