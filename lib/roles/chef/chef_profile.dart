c030ef6cb6b2459c454e9a2396fcb8223fe18431
Balavardhan Reddy Malyala <62328664+balavardhan224@users.noreply.github.com>
2022-03-25 01:47:06 +0530

    chef profilepic stored in storage

--- lib/roles/chef/chef_profile.dart
+++ lib/roles/chef/chef_profile.dart
@@ -1,7 +1,11 @@
+import 'dart:io';
+
 import 'package:chef_connect_india/Helper/model.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 
 class ProfilePageUser extends StatefulWidget {
   String? documentId;
@@ -89,7 +93,6 @@ class MapScreenState extends State<ProfilePageUser>
       // print(value['aadharNumber']);
 
       oldFullName = value['firstname'];
-      oldMobileNumber = value['dob'];
       // oldemail: value['email'];
       // oldlastname: value['lastname'];
       // olddob: value['dob'];
@@ -122,6 +125,26 @@ class MapScreenState extends State<ProfilePageUser>
       _controller1.text = oldMobileNumber.toString();
     });
     _controller = new TextEditingController(text: oldFullName);
+  }
+  File? image;
+  final imagePicker = ImagePicker();
+  late String downloadURL;
+  Future ImagePickerMethod() async {
+    // ignore: unused_local_variable
+    final pick = await imagePicker.pickImage(source: ImageSource.gallery);
+    setState(() {
+      if (pick != null) {
+        image=File(pick.path);
+      }
+    else {}
+    });
+    Reference ref= FirebaseStorage.instance.ref().child("profileURL's").child(documentId!);
+    FirebaseFirestore firebaseFirestore= FirebaseFirestore.instance;
+    await ref.putFile(image!);
+    downloadURL= await ref.getDownloadURL();
+    if (downloadURL != null ) {
+         Firebasehelper.updatepic(documentId, downloadURL);
+                    }
   }
   @override
   Widget build(BuildContext context) {
@@ -179,12 +202,17 @@ class MapScreenState extends State<ProfilePageUser>
                             child: Row(
                               mainAxisAlignment: MainAxisAlignment.center,
                               children: <Widget>[
-                                CircleAvatar(
-                                  backgroundColor: Colors.red,
-                                  radius: 25.0,
-                                  child: Icon(
-                                    Icons.camera_alt,
-                                    color: Colors.white,
+                                GestureDetector(
+                                  onTap: () {
+                                     ImagePickerMethod();
+                                  },
+                                  child: CircleAvatar(
+                                    backgroundColor: Colors.red,
+                                    radius: 25.0,
+                                    child: Icon(
+                                      Icons.camera_alt,
+                                      color: Colors.white,
+                                    ),
                                   ),
                                 )
                               ],
@@ -277,49 +305,6 @@ class MapScreenState extends State<ProfilePageUser>
                               ),
                             ],
                           )),
-                      Padding(
-                          padding: EdgeInsets.only(
-                              left: 25.0, right: 25.0, top: 25.0),
-                          child: Row(
-                            mainAxisSize: MainAxisSize.max,
-                            children: <Widget>[
-                              Column(
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                mainAxisSize: MainAxisSize.min,
-                                children: <Widget>[
-                                  Text(
-                                    'Mobile',
-                                    style: TextStyle(
-                                        fontSize: 16.0,
-                                        fontWeight: FontWeight.bold),
-                                  ),
-                                ],
-                              ),
-                            ],
-                          )),
-                      Padding(
-                          padding: EdgeInsets.only(
-                              left: 25.0, right: 25.0, top: 2.0),
-                          child: Row(
-                            mainAxisSize: MainAxisSize.max,
-                            children: <Widget>[
-                              Flexible(
-                                child: TextField(
-                                    controller: _controller1,
-                                    decoration: const InputDecoration(
-                                        hintText: "Enter Mobile Number"),
-                                    enabled: !_status,
-                                    onChanged: (value) {
-                                      setState(() {
-                                        newMobileNumber = value;
-                                      });
-                                      newMobileNumber == null
-                                          ? newMobileNumber = oldMobileNumber
-                                          : newMobileNumber = newMobileNumber;
-                                    }),
-                              ),
-                            ],
-                          )),
                       !_status ? _getActionButtons() : Container(),
                     ],
                   ),
@@ -356,15 +341,17 @@ class MapScreenState extends State<ProfilePageUser>
                 color: Colors.green,
                 onPressed: () {
                   //Firebasehelper.updateAppointment(documentId, newFullName);
+                  print('==========;');
                   print(documentId);
+                  //Firebasehelper.updatemsg(documentId, newFullName);
                   // var a='4kPFZNe1JbYyF3hJ4omFks6MMbN2';
-                  // Firebasehelper.updateAppointment(documentId, "confirmed");
+                  //Firebasehelper.updatemsg(documentId, "confirmed");
                   // handleUpdateData();
                   // print(newFullName);
                   setState(() {
-                    if (newFullName != null && newMobileNumber != null) {
+                    if (newFullName != null ) {
                       Firebasehelper.updatemsg(documentId, newFullName);
-                      print(documentId);
+                      // print(documentId);
                       // var firebaseUser = FirebaseAuth.instance.currentUser;
                       // FirebaseFirestore.instance
                       //     .collection("chefs")
