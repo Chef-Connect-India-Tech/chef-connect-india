c92a333631f6b69b3b071d3ee7713c35e267e829
Aravindreddy Mokireddy <aravindreddy.mokireddy@gmail.com>
2022-03-30 23:53:05 +0530

    minor changes

--- lib/main.dart
+++ lib/main.dart
@@ -39,24 +39,6 @@ class ChefConnectMain extends StatefulWidget {
 }
 
 class ChefConnectMainState extends State<ChefConnectMain> {
-  // @override
-  void initstate() {
-    getValidationData().whenComplete(() async {
-      Timer(Duration(seconds: 2), () => Select_Mode());
-    });
-    super.initState();
-  }
-
-  Future getValidationData() async {
-    final SharedPreferences sharedPreferences =
-        await SharedPreferences.getInstance();
-    var obtainedPhone = sharedPreferences.getString('mobile1');
-    setState(() {
-      finalPhone = obtainedPhone;
-    });
-    print(finalPhone);
-  }
-
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
