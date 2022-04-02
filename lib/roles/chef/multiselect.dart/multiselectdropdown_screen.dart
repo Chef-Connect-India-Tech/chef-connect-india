8494a3b253f0cefa4b4867a2f073c677d4bf702d
balavardhan224 <balavardhanrdy@gmail.com>
2022-04-02 22:16:40 +0530

    multi select

--- lib/roles/chef/multiselect.dart/multiselectdropdown_screen.dart
+++ lib/roles/chef/multiselect.dart/multiselectdropdown_screen.dart
@@ -1,247 +1,141 @@
-// import 'package:flutter/material.dart';
-// import 'package:multi_select_flutter/multi_select_flutter.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
 
-// void main() {
-//   runApp(MyApp());
-// }
+class MyHomePage1 extends StatefulWidget {
+  MyHomePage1({Key? key, required this.title}) : super(key: key);
 
-// class MyApp extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Flutter Multi Select',
-//       theme: ThemeData(
-//         primarySwatch: Colors.purple,
-//         visualDensity: VisualDensity.adaptivePlatformDensity,
-//       ),
-//       home: MyHomePage(title: 'Flutter Multi Select'),
-//     );
-//   }
-// }
+  final String title;
 
-// class Animal {
-//   final int id;
-//   final String name;
+  @override
+  _MyHomePage1State createState() => _MyHomePage1State();
+}
 
-//   Animal({
-//     required this.id,
-//     required this.name,
-//   });
-// }
+class _MyHomePage1State extends State<MyHomePage1> {
+  List favoriteMovies1 = [];
+  Future<void> getData() async {
+    var docc = 'pDSWbcECuimgt7HS6Xi6';
+    // Get docs from collection reference
+    FirebaseFirestore.instance
+        .collection('Menu')
+        .doc(docc)
+        .collection("starters")
+        .get()
+        .then((snapshot) => {
+              snapshot.docs.forEach((doc) {
+                favoriteMovies1.add(doc['dish']);
+              })
+            });
+    print(favoriteMovies1);
+  }
 
-// class MyHomePage extends StatefulWidget {
-//   MyHomePage({Key? key, required this.title}) : super(key: key);
-//   final String title;
-//   @override
-//   _MyHomePageState createState() => _MyHomePageState();
-// }
+  List<String> selectedReportList = [];
 
-// class _MyHomePageState extends State<MyHomePage> {
-//   static List<Animal> _animals = [
-//     Animal(id: 1, name: "Lion"),
-//     Animal(id: 2, name: "Flamingo"),
-//     Animal(id: 3, name: "Hippo"),
-//     Animal(id: 4, name: "Horse"),
-//     Animal(id: 5, name: "Tiger"),
-//     Animal(id: 6, name: "Penguin"),
-//     Animal(id: 7, name: "Spider"),
-//     Animal(id: 8, name: "Snake"),
-//     Animal(id: 9, name: "Bear"),
-//     Animal(id: 10, name: "Beaver"),
-//     Animal(id: 11, name: "Cat"),
-//     Animal(id: 12, name: "Fish"),
-//     Animal(id: 13, name: "Rabbit"),
-//     Animal(id: 14, name: "Mouse"),
-//     Animal(id: 15, name: "Dog"),
-//     Animal(id: 16, name: "Zebra"),
-//     Animal(id: 17, name: "Cow"),
-//     Animal(id: 18, name: "Frog"),
-//     Animal(id: 19, name: "Blue Jay"),
-//     Animal(id: 20, name: "Moose"),
-//     Animal(id: 21, name: "Gecko"),
-//     Animal(id: 22, name: "Kangaroo"),
-//     Animal(id: 23, name: "Shark"),
-//     Animal(id: 24, name: "Crocodile"),
-//     Animal(id: 25, name: "Owl"),
-//     Animal(id: 26, name: "Dragonfly"),
-//     Animal(id: 27, name: "Dolphin"),
-//   ];
-//   final _items = _animals
-//       .map((animal) => MultiSelectItem<Animal>(animal, animal.name))
-//       .toList();
-//   //List<Animal> _selectedAnimals = [];
-//   List<Animal> _selectedAnimals2 = [];
-//   List<Animal> _selectedAnimals3 = [];
-//   //List<Animal> _selectedAnimals4 = [];
-//   List<Animal> _selectedAnimals5 = [];
-//   final _multiSelectKey = GlobalKey<FormFieldState>();
+  _showReportDialog() {
+    var docc = 'pDSWbcECuimgt7HS6Xi6';
+    // Get docs from collection reference
+    FirebaseFirestore.instance
+        .collection('Menu')
+        .doc(docc)
+        .collection("starters")
+        .get()
+        .then((snapshot) => {
+              snapshot.docs.forEach((doc) {
+                favoriteMovies1.add(doc['dish']);
+              })
+            });
+    print(favoriteMovies1);
+    List<String> reportList1 = favoriteMovies1.cast<String>().toSet().toList();
+    print(reportList1);
 
-//   @override
-//   void initState() {
-//     _selectedAnimals5 = _animals;
-//     super.initState();
-//   }
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          //Here we will build the content of the dialog
+          return AlertDialog(
+            title: Text("Select Starters"),
+            content: MultiSelectChip(
+              reportList1,
+              onSelectionChanged: (selectedList) {
+                setState(() {
+                  selectedReportList = selectedList;
+                });
+              },
+            ),
+            actions: <Widget>[
+              FlatButton(
+                child: Text("Select"),
+                onPressed: () => Navigator.of(context).pop(),
+              )
+            ],
+          );
+        });
+  }
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text(widget.title),
-//       ),
-//       body: SingleChildScrollView(
-//         child: Container(
-//           alignment: Alignment.center,
-//           padding: EdgeInsets.all(20),
-//           child: Column(
-//             children: <Widget>[
-//               SizedBox(height: 40),
-//               //################################################################################################
-//               // Rounded blue MultiSelectDialogField
-//               //################################################################################################
-//               MultiSelectDialogField(
-//                 items: _items,
-//                 title: Text("Animals"),
-//                 selectedColor: Colors.blue,
-//                 decoration: BoxDecoration(
-//                   color: Colors.blue.withOpacity(0.1),
-//                   borderRadius: BorderRadius.all(Radius.circular(40)),
-//                   border: Border.all(
-//                     color: Colors.blue,
-//                     width: 2,
-//                   ),
-//                 ),
-//                 buttonIcon: Icon(
-//                   Icons.pets,
-//                   color: Colors.blue,
-//                 ),
-//                 buttonText: Text(
-//                   "Favorite Animals",
-//                   style: TextStyle(
-//                     color: Colors.blue[800],
-//                     fontSize: 16,
-//                   ),
-//                 ),
-//                 onConfirm: (results) {
-//                   //_selectedAnimals = results;
-//                 },
-//               ),
-//               SizedBox(height: 50),
-//               //################################################################################################
-//               // This MultiSelectBottomSheetField has no decoration, but is instead wrapped in a Container that has
-//               // decoration applied. This allows the ChipDisplay to render inside the same Container.
-//               //################################################################################################
-//               Container(
-//                 decoration: BoxDecoration(
-//                   color: Theme.of(context).primaryColor.withOpacity(.4),
-//                   border: Border.all(
-//                     color: Theme.of(context).primaryColor,
-//                     width: 2,
-//                   ),
-//                 ),
-//                 child: Column(
-//                   children: <Widget>[
-//                     MultiSelectBottomSheetField(
-//                       initialChildSize: 0.4,
-//                       listType: MultiSelectListType.CHIP,
-//                       searchable: true,
-//                       buttonText: Text("Favorite Animals"),
-//                       title: Text("Animals"),
-//                       items: _items,
-//                       onConfirm: (values) {
-//                         _selectedAnimals2 = values;
-//                       },
-//                       chipDisplay: MultiSelectChipDisplay(
-//                         onTap: (value) {
-//                           setState(() {
-//                             _selectedAnimals2.remove(value);
-//                           });
-//                         },
-//                       ),
-//                     ),
-//                     _selectedAnimals2 == null || _selectedAnimals2.isEmpty
-//                         ? Container(
-//                             padding: EdgeInsets.all(10),
-//                             alignment: Alignment.centerLeft,
-//                             child: Text(
-//                               "None selected",
-//                               style: TextStyle(color: Colors.black54),
-//                             ))
-//                         : Container(),
-//                   ],
-//                 ),
-//               ),
-//               SizedBox(height: 40),
-//               //################################################################################################
-//               // MultiSelectBottomSheetField with validators
-//               //################################################################################################
-//               MultiSelectBottomSheetField<Animal>(
-//                 key: _multiSelectKey,
-//                 initialChildSize: 0.7,
-//                 maxChildSize: 0.95,
-//                 title: Text("Animals"),
-//                 buttonText: Text("Favorite Animals"),
-//                 items: _items,
-//                 searchable: true,
-//                 validator: (values) {
-//                   if (values == null || values.isEmpty) {
-//                     return "Required";
-//                   }
-//                   List<String> names = values.map((e) => e.name).toList();
-//                   if (names.contains("Frog")) {
-//                     return "Frogs are weird!";
-//                   }
-//                   return null;
-//                 },
-//                 onConfirm: (values) {
-//                   setState(() {
-//                     _selectedAnimals3 = values;
-//                   });
-//                   _multiSelectKey.currentState!.validate();
-//                 },
-//                 chipDisplay: MultiSelectChipDisplay(
-//                   onTap: (item) {
-//                     setState(() {
-//                       _selectedAnimals3.remove(item);
-//                     });
-//                     _multiSelectKey.currentState!.validate();
-//                   },
-//                 ),
-//               ),
-//               SizedBox(height: 40),
-//               //################################################################################################
-//               // MultiSelectChipField
-//               //################################################################################################
-//               MultiSelectChipField(
-//                 items: _items,
-//                 initialValue: [_animals[4], _animals[7], _animals[9]],
-//                 title: Text("Animals"),
-//                 headerColor: Colors.blue.withOpacity(0.5),
-//                 decoration: BoxDecoration(
-//                   border: Border.all(color: Colors.blue, width: 1.8),
-//                 ),
-//                 selectedChipColor: Colors.blue.withOpacity(0.5),
-//                 selectedTextStyle: TextStyle(color: Colors.blue[800]),
-//                 onTap: (values) {
-//                   //_selectedAnimals4 = values;
-//                 },
-//               ),
-//               SizedBox(height: 40),
-//               //################################################################################################
-//               // MultiSelectDialogField with initial values
-//               //################################################################################################
-//               MultiSelectDialogField(
-//                 onConfirm: (val) {
-//                   _selectedAnimals5 = val;
-//                 },
-//                 dialogWidth: MediaQuery.of(context).size.width * 0.7,
-//                 items: _items,
-//                 initialValue:
-//                     _selectedAnimals5, // setting the value of this in initState() to pre-select values.
-//               ),
-//             ],
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            RaisedButton(
+              child: Text("Select"),
+              onPressed: () => _showReportDialog(),
+            ),
+            Text(selectedReportList.join(" , ")),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class MultiSelectChip extends StatefulWidget {
+  final List<String> reportList1;
+  final Function(List<String>) onSelectionChanged;
+
+  MultiSelectChip(this.reportList1, {required this.onSelectionChanged});
+
+  @override
+  _MultiSelectChipState createState() => _MultiSelectChipState();
+}
+
+class _MultiSelectChipState extends State<MultiSelectChip> {
+  // String selectedChoice = "";
+  List<String> selectedChoices = [];
+
+  _buildChoiceList() {
+    List<Widget> choices = [];
+
+    widget.reportList1.forEach((item) {
+      choices.add(Container(
+        padding: const EdgeInsets.all(2.0),
+        child: ChoiceChip(
+          label: Text(item),
+          selected: selectedChoices.contains(item),
+          onSelected: (selected) {
+            setState(() {
+              selectedChoices.contains(item)
+                  ? selectedChoices.remove(item)
+                  : selectedChoices.add(item);
+              widget.onSelectionChanged(selectedChoices);
+            });
+          },
+        ),
+      ));
+    });
+
+    return choices;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Wrap(
+      children: _buildChoiceList(),
+    );
+  }
+}
